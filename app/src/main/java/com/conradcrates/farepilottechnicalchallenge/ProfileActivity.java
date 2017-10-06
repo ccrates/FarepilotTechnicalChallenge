ad6844086a8e1d15e5e8c4aa6df82f882ad79325
Conrad Crates <conrad-crates@hotmail.co.uk>
2017-10-06 16:13:20 +0100

    Added camera intent when tapping on profile picture

--- app/src/main/java/com/conradcrates/farepilottechnicalchallenge/ProfileActivity.java
+++ app/src/main/java/com/conradcrates/farepilottechnicalchallenge/ProfileActivity.java
@@ -1,13 +1,21 @@
 package com.conradcrates.farepilottechnicalchallenge;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.conradcrates.farepilottechnicalchallenge.backend.NetworkResponse;
 import com.conradcrates.farepilottechnicalchallenge.backend.RestClientFactory;
+import com.conradcrates.farepilottechnicalchallenge.constants.IntentConstants;
 import com.conradcrates.farepilottechnicalchallenge.constants.NetworkResponseConstants;
 import com.conradcrates.farepilottechnicalchallenge.gravatar.Gravatar;
 
@@ -27,14 +35,57 @@ public class ProfileActivity extends AppCompatActivity {
         String email = response.getValue(NetworkResponseConstants.EMAIL);
 
         username.setText(email);
-        String url = Gravatar.createGravatarUrl(email);
 
+        String url = Gravatar.createGravatarUrl(email);
         Glide.with(getApplicationContext()).load(url).into(avatar);
+
+        avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = avatarDialog();
+                dialog.show();
+            }
+        });
     }
 
     private void initViews(){
         username = findViewById(R.id.text_username);
         password = findViewById(R.id.text_password);
         avatar = findViewById(R.id.image_avatar);
+    }
+
+    private Dialog avatarDialog() {
+        CharSequence[] items = new CharSequence[]{
+                getString(R.string.profile_dialog_avatar_camera),
+                getString(R.string.profile_dialog_avatar_gallery)};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.profile_dialog_avatar_title)
+                .setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+
+                        switch (i){
+                            case 0:
+                                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                if(intent.resolveActivity(getPackageManager()) != null){
+                                    startActivityForResult(intent, IntentConstants.REQUEST_IMAGE_CAPTURE);
+                                }
+                                break;
+                            case 1:
+                                break;
+                        }
+                    }
+                });
+        return builder.create();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == IntentConstants.REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap img = (Bitmap)extras.get(IntentConstants.CAMERA_DATA);
+        }
     }
 }
---
+++ app/src/main/java/com/conradcrates/farepilottechnicalchallenge/constants/IntentConstants.java
@@ -0,0 +1,12 @@
+package com.conradcrates.farepilottechnicalchallenge.constants;
+
+/**
+ * Created by Conrad on 06/10/2017.
+ */
+
+public class IntentConstants {
+
+    public static final int REQUEST_IMAGE_CAPTURE = 1001;
+
+    public static final String CAMERA_DATA = "data";
+}
